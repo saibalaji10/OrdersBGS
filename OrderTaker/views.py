2a1566b37661f1e100c573b4474c823cafb67be3
chandeesh <taekchan331@gmail.com>
2020-06-28 23:01:06 +0530

    Return alert

--- OrderTaker/views.py
+++ OrderTaker/views.py
@@ -9,7 +9,16 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def index(request):
-    category_list = Category.objects.order_by('name')
+    pa_list = ProductAttribute.objects.all()
+
+    distinct_prod_list = pa_list.values_list('product').distinct()
+    product_list = Product.objects.filter(id__in=distinct_prod_list).order_by('name')
+
+    distinct_category_list = Product.objects.filter(id__in=distinct_prod_list).values_list('category').distinct()
+    category_list = Category.objects.filter(id__in=distinct_category_list).order_by('name')
+
+    attribute_list = Attribute.objects.order_by('name')
+
     page = request.GET.get('page', 1)
 
     paginator = Paginator(category_list, 5)
@@ -23,34 +32,12 @@ def index(request):
 
     context = {
         'category_list': categories,
-    }
-    return render(request, 'OrderTaker/index.html', context)
-
-
-def index2(request):
-    pa_list = ProductAttribute.objects.all()
-
-    distinct_prod_list = pa_list.values_list('product').distinct()
-    product_list = Product.objects.filter(id__in=distinct_prod_list).order_by('name')
-
-    distinct_category_list = Product.objects.filter(id__in=distinct_prod_list).values_list('category').distinct()
-    category_list = Category.objects.filter(id__in=distinct_category_list).order_by('name')
-
-    attribute_list = Attribute.objects.order_by('name')
-
-    print(product_list)
-    print(category_list)
-    print(attribute_list)
-    print(pa_list)
-    context = {
-        'category_list': category_list,
         'product_list': product_list,
         'attribute_list': attribute_list,
         'pa_list': pa_list,
         'message': request.session['message'] if 'message' in request.session else None
     }
-    return render(request, 'OrderTaker/index2.html', context)
-
+    return render(request, 'OrderTaker/index.html', context)
 
 def addtocart(request):
     try:
@@ -106,4 +93,4 @@ def addtocart(request):
     request.session['customer_id'] = cust.id
     request.session['order_id'] = cust_order.id
 
-    return HttpResponseRedirect(reverse('index'))
+    return HttpResponseRedirect(reverse('index2'))
