67283a06ba6afde570d700f1702ede237e6a1158
Sai Balaji <baju@MacBook-Pro.local>
2020-08-10 20:06:59 +0530

    Created orders page and modified some models

--- OrderTaker/admin.py
+++ OrderTaker/admin.py
@@ -1,15 +1,97 @@
-from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from django.contrib.admin import SimpleListFilter
-from .models import Order, Customer, OrderDetails, ProductAttribute, Config, Category, Attribute
+from .models import Order, OrderDetails, ProductAttribute, Config, Category, Attribute
 from import_export import resources, fields
 from import_export.admin import ImportExportModelAdmin
 from import_export.widgets import ForeignKeyWidget
 
+from django import forms
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
-#
+from .models import BGSUser
+
+
+class UserCreationForm(forms.ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = BGSUser
+        fields = ('name', 'phone')
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    password hash display field.
+    """
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = BGSUser
+        fields = ('phone', 'password', 'is_active', 'is_admin')
+
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]
+
+
+class BGSUserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('name', 'phone', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('phone', 'password')}),
+        ('Personal info', {'fields': ('name',)}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('name', 'phone', 'password1', 'password2')}
+         ),
+    )
+    search_fields = ('phone',)
+    ordering = ('phone',)
+    filter_horizontal = ()
+
+
+# Now register the new UserAdmin...
+admin.site.register(BGSUser, BGSUserAdmin)
+# ... and, since we're not using Django's built-in permissions,
+# unregister the Group model from admin.
+admin.site.unregister(Group)
+
+
 class ProductResources(resources.ModelResource):
 
     def before_import_row(self, row, **kwargs):
@@ -19,10 +101,11 @@ class ProductResources(resources.ModelResource):
         attributeobject, created = Attribute.objects.get_or_create(
             name=row.get('attribute')
         )
+
     category = fields.Field(column_name='category', attribute='category', widget=ForeignKeyWidget(Category, 'name'))
     attribute = fields.Field(column_name='attribute', attribute='attribute', widget=ForeignKeyWidget(Attribute, 'name'))
 
-    product = fields.Field(column_name='product', attribute='product',)
+    product = fields.Field(column_name='product', attribute='product', )
 
     class Meta:
         model = ProductAttribute
@@ -30,6 +113,7 @@ class ProductResources(resources.ModelResource):
         report_skipped = True
         exclude = ('id', 'category')
         import_id_fields = ('product', 'attribute')
+
 
 class ConfigResources(resources.ModelResource):
 
@@ -46,6 +130,7 @@ class ConfigResources(resources.ModelResource):
         skip_unchanged = True
         report_skipped = True
         exclude = ('id')
+
 
 class OrderAdmin(admin.ModelAdmin):
     # ...
@@ -65,11 +150,10 @@ class ConfigAdmin(ImportExportModelAdmin):
 class OrderDetailsAdmin(admin.ModelAdmin):
     list_display = ('order', 'product_attribute', 'quantity')
 
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name',)
 
-class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'number', 'id')
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
 
 class AttributeAdmin(admin.ModelAdmin):
     list_display = ('id', 'name',)
@@ -95,7 +179,6 @@ class ProductAttributeAdmin(ImportExportModelAdmin):
 admin.site.site_header = 'Bombay General Stores Admin'
 admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderDetails, OrderDetailsAdmin)
-admin.site.register(Customer, CustomerAdmin)
 admin.site.register(Config, ConfigAdmin)
 admin.site.register(ProductAttribute, ProductAttributeAdmin)
 admin.site.register(Category, CategoryAdmin)
